b76cea38c8b20f7575f52f67b3322851ec98fd88
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-03-10 16:01:15 -0300

    Classe Aluno

--- signin_java_oop/src/signin_java_oop/aluno.java
+++ signin_java_oop/src/signin_java_oop/aluno.java
@@ -1,10 +1,79 @@
+/* 
+ * 
+ * @Author: MateusLeviDev
+ * */
+
 package signin_java_oop;
 
+import java.util.ArrayList;
+
 public class aluno {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// criação do construtor aluno alt+shift+s constructor using fields
+	
+	private String nome, cpf, situaçao = "reprovado";
+	ArrayList<Float> notasAluno = new ArrayList<Float>();
+	float total = 0;
+	
+	public ArrayList<Float> getNotasAluno() {
+		return notasAluno;
 	}
 
+	public void setNotasAluno(ArrayList<Float> notasAluno) {
+		this.notasAluno = notasAluno;
+	}
+
+	public aluno() {} //vazio não precisa repassar dados.
+	
+	public aluno(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	void insereNotas(float grades) {
+		notasAluno.add(grades);
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	void listar() {
+		System.out.println("Nome: " + this.nome);
+		System.out.println("CPF: " + this.cpf);
+		
+		for (int i = 0; 1 < notasAluno.size(); i++) {
+			System.out.println("Nota " + (i + 1) + ": " + this.notasAluno.get(i));
+		}
+	}
+	
+	void situacao() {
+		System.out.println("Nome: " + this.nome);
+		System.out.println("Total: " + this.total);
+		System.out.println("Situação: " + this.situaçao);
+		
+	}
+	
+	void sumGrade() {
+		for (float notaDaLista:notasAluno) {
+			total += notaDaLista;
+		}
+		
+		if (total >= 70) {
+			this.situaçao = "aprovado";
+		} else {
+			this.situaçao = "reprovado";
+		}
+	}
 }
