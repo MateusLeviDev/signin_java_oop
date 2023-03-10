a265536b4f86912c52f98115ca455909ac2c5855
MateusLeviDev <mateuslevisouzapereira@gmail.com>
2023-03-10 16:01:31 -0300

    class sala

--- signin_java_oop/src/signin_java_oop/sala.java
+++ signin_java_oop/src/signin_java_oop/sala.java
@@ -1,10 +1,37 @@
+/* 
+ * 
+ * @Author: MateusLeviDev
+ * */
+
 package signin_java_oop;
+import java.util.ArrayList;
+
 
 public class sala {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	ArrayList<aluno> ListaDeAlunos;
+	
+	public sala() {
+			this.ListaDeAlunos = new ArrayList<>();
 	}
 
+	void insereAluno(aluno novoRegristro) {
+		novoRegristro.sumGrade();
+		ListaDeAlunos.add(novoRegristro);
+	}
+	
+	void listarAlunos() {
+		System.out.println("Lista de alunos: ");
+		
+		for (aluno a:ListaDeAlunos) { //var a do tipo aluno
+			a.listar();
+		}
+	}
+	
+	void listarSituacao() {
+		System.out.println("Status: ");
+		for (aluno alunoNaLista:ListaDeAlunos) {
+			alunoNaLista.situacao();
+		}
+	}
+	
 }
